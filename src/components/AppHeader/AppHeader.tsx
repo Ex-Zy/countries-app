ce7ba0b9613534fdde5caf7c08e3382fddc9459f
Sergey Nagornykh <serega1905@ukr.net>
2024-05-31 11:40:41 +0300

    :star: new(components): AppHeader

---
+++ src/components/AppHeader/AppHeader.tsx
@@ -0,0 +1,11 @@
+import './AppHeader.scss'
+
+export const AppHeader = () => {
+  return (
+    <header className="app-header">
+      <div className="container app-header__in">
+        <h1 className="app-header__title">Where in the world?</h1>
+      </div>
+    </header>
+  )
+}
--- src/components/Countries/Countries.tsx
+++ src/components/Countries/Countries.tsx
@@ -3,16 +3,20 @@ import './Countries.scss'
 import { FilterBar } from '../FilterBar/FilterBar.tsx'
 import { CountryList } from '../CountryList/CountryList.tsx'
 import { useCountryFilter } from '../../hooks/useCountryFilter.ts'
+import { AppHeader } from '../AppHeader/AppHeader.tsx'
 
 export const Countries = () => {
   const { formFilterState, countryFilter, handleFormFilterChange } = useCountryFilter()
 
   return (
-    <div className="countries">
-      <div className="container">
-        <FilterBar form={formFilterState} onChange={handleFormFilterChange} />
-        <CountryList filter={countryFilter} />
+    <>
+      <AppHeader />
+      <div className="countries">
+        <div className="container">
+          <FilterBar form={formFilterState} onChange={handleFormFilterChange} />
+          <CountryList filter={countryFilter} />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
